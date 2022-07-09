78062772e0fa7bb9645907ffb108460b797dd3bc
Fredric Silberberg <fred@silberberg.xyz>
2022-07-09 14:56:16 -0700

    Initial commit

---
+++ test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MangaReleaseRsser.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
