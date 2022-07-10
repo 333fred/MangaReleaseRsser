d58f216676e5b57e987121db84d01d13a6312ddf
Fredric Silberberg <fred@silberberg.xyz>
2022-07-09 18:46:15 -0700

    Added support for Yen Press's calendar.

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,108 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+if (args is not [string path])
+{
+    throw new Exception("Must provide a path to the RSS file.");
+}
+
+var allReleases = new Dictionary<string, List<MangaRelease>>();
+
+try
+{
+    using var xmlReader = XmlReader.Create(path);
+    var feed = SyndicationFeed.Load(xmlReader);
+
+    var groupedItems = feed.Items.Select(MangaRelease.ToMangaRelease).GroupBy(x => x.Title);
+
+    foreach (var group in groupedItems)
+    {
+        allReleases.Add(group.Key, group.ToList());
+    }
+}
+catch (FileNotFoundException)
+{ }
+
+var publishers = new IPublisher[] {
+    new YenPress(),
+};
+
+var today = DateOnly.FromDateTime(DateTime.Today);
+var thirtyDaysAgo = today.AddDays(-30);
+
+foreach (var publisher in publishers)
+{
+    Console.WriteLine($"Processing {publisher.Name}");
+
+    var releases = await publisher.GetReleasesForDate(today);
+    if (allReleases.TryGetValue(publisher.Name, out var existingReleases))
+    {
+        allReleases[publisher.Name].AddRange(releases);
+    }
+    else
+    {
+        allReleases.Add(publisher.Name, releases);
+    }
+
+    for (int i = 0; i < releases.Count; i++)
+    {
+        if (releases[i].ReleaseDate < thirtyDaysAgo)
+        {
+            releases.RemoveAt(i);
+            i--;
+            continue;
+        }
+        break;
+    }
+}
+
+var items = allReleases.SelectMany(x => x.Value).OrderBy(x => x.ReleaseDate).Select(MangaRelease.ToSyndicationItem).ToList();
+var syndicationFeed = new SyndicationFeed(
+    "Official Manga Releases",
+    "Publication information for new official manga publications",
+    new Uri("https://manga.silberberg.xyz/rss.xml"),
+    items);
+
+using var writer = XmlWriter.Create(path);
+var rssFormatter = new Rss20FeedFormatter(syndicationFeed);
+rssFormatter.WriteTo(writer);
+
+interface IPublisher
+{
+    Task<List<MangaRelease>> GetReleasesForDate(DateOnly date);
+    string Name { get; }
+}
+
+public record MangaRelease(string Title, string Author, string Description, DateOnly ReleaseDate, string Price, Uri ReleaseUrl, Uri ImageUrl)
+{
+    public static SyndicationItem ToSyndicationItem(MangaRelease release)
+        => new(
+            title: release.Title,
+            content: "",
+            itemAlternateLink: release.ReleaseUrl)
+        {
+            Summary = new TextSyndicationContent($"""
+                     Title: {release.Title}
+                     Author: {release.Author}
+                     Description: {release.Description}
+                     Release Date: {release.ReleaseDate:d}
+                     Price: {release.Price}
+                     """),
+            ElementExtensions = {
+                { "thumbnail", "media", release.ImageUrl.ToString() }
+            }
+        };
+
+    public static MangaRelease ToMangaRelease(SyndicationItem item)
+    {
+        var title = item.Title.Text;
+        var descriptionLines = item.Summary.Text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        var author = descriptionLines[1]["Author: ".Length..].Trim();
+        var description = descriptionLines[2]["Description: ".Length..].Trim();
+        var releaseDate = DateOnly.Parse(descriptionLines[3]["Release Date: ".Length..].Trim());
+        var price = descriptionLines[4]["Price: ".Length..].Trim();
+        var releaseUrl = item.BaseUri;
+        var imageUrl = new Uri(item.ElementExtensions.FirstOrDefault(x => x.OuterName == "thumbnail")!.GetReader().GetAttribute("url")!);
+        return new MangaRelease(title, author, description, releaseDate, price, releaseUrl, imageUrl);
+    }
+}
---
+++ src/Publishers/YenPress.cs
@@ -0,0 +1,60 @@
+using HtmlAgilityPack;
+
+public class YenPress : IPublisher
+{
+    public string Name => "Yen Press";
+
+    private const string YenPressBaseUrl = "https://yenpress.com";
+    private const string DigitalReleaseLink = $"{YenPressBaseUrl}/digital/";
+
+    public async Task<List<MangaRelease>> GetReleasesForDate(DateOnly date)
+    {
+        var web = new HtmlWeb();
+        var doc = await web.LoadFromWebAsync(DigitalReleaseLink);
+
+        var outThisMonth = doc.DocumentNode.SelectSingleNode("""//*[@id="yen-press-0"]""");
+
+        var releases = new List<MangaRelease>();
+
+        var today = DateOnly.FromDateTime(DateTime.Today);
+
+        foreach (var bookLi in outThisMonth.SelectNodes("""li"""))
+        {
+            var bookLink = bookLi.SelectSingleNode("""div/div[@class="book-detail-links"]/a""").GetAttributeValue("href", null);
+            if (bookLink is null)
+            {
+                Console.WriteLine($"Failed to retrieve book link for {bookLi}");
+                continue;
+            }
+
+            bookLink = $"{YenPressBaseUrl}{bookLink}";
+
+            var bookDoc = await web.LoadFromWebAsync(bookLink);
+            var title = bookDoc.DocumentNode.SelectSingleNode("""//*[@id="book-title"]""").InnerText;
+
+            Console.WriteLine($"Processing title {title}");
+
+            const string FullDetailsXpath = """//div[@id="book-details"]/ul""";
+            var releaseDate = DateOnly.Parse(bookDoc.DocumentNode.SelectSingleNode($"""{FullDetailsXpath}/li[last()]/span[last()]""").InnerText);
+
+            if (releaseDate > today)
+            {
+                // Releases are alphabetically ordered, so we have to keep looking.
+                Console.WriteLine($"Skipping {title} because it is not for today");
+                continue;
+            }
+
+            var author = bookDoc.DocumentNode.SelectSingleNode("""//h3[@id="book-author"]""").InnerText;
+            // For some reason, HtmlAgilityPack is seeing the h3 tag as having the description div as its child node. It's not, but just work around it.
+            author = author[..author.IndexOf('\n')];
+            var description = bookDoc.DocumentNode.SelectSingleNode("""//*[@id="book-description-full"]""").InnerText.ReplaceLineEndings().Replace(Environment.NewLine, " ");
+            var imageUrl = bookDoc.DocumentNode.SelectSingleNode("""//*[@id="main-cover"]/picture/img""").GetAttributeValue("src", null);
+            var price = bookDoc.DocumentNode.SelectSingleNode($"""{FullDetailsXpath}/span[last()]""").InnerText;
+            price = price[(price.IndexOf(": ") + 2)..];
+
+            releases.Add(new(title, author, description, releaseDate, price, new Uri(bookLink), new Uri(imageUrl)));
+        }
+
+        return releases;
+    }
+}
